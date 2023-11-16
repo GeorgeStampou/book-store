3551a6b33de7981adee5f371a73ce1329cb9aaaf
George Stampoulidis <georgios.stampoulidis@gmail.com>
2023-11-16 15:58:36 +0200

    axios works fine front get backend data

--- app.js
+++ app.js
@@ -9,6 +9,26 @@ const app = express();
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  // Website to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  // Request methods wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  // Request headers you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
+
 app.use('/api/v1/books', router);
 
 const port = process.env.PORT || 3000;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3000';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const getBooks = async () => {
+    try {
+      const response = await axios.get('/api/v1/books');
+      const { data } = response;
+      // console.log(data);
+      // console.log(data.books);
+      setBooks(data.books);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getBooks();
+  }, []);
+
   return (
     <>
       <div>Hello from react + nodejs</div>
+      <div>f</div>
     </>
   );
 }
