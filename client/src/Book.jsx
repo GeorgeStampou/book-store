f0369a7c2e5b19c923d484833e22bb7e3c41f2e4
George Stampoulidis <georgios.stampoulidis@gmail.com>
2023-11-17 19:37:39 +0200

    photo and title link to full info book page

--- client/src/Book.jsx
+++ client/src/Book.jsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import './Book.css';
+import FullBook from './FullBook';
 
 const Book = ({
   _id: id,
@@ -13,24 +15,27 @@ const Book = ({
   const authorsWithSpace = authors.join(', ');
 
   return (
-    <article>
-      <div className='book-poster'>
-        <img src={cover} alt={title} />
-        <div className='book-info'>
-          <p>
-            Author(s): {authorsWithSpace} <br />
-            Genre: {genre}
-            <br />
-            Publication date: {publicationDate}
-            <br />
-            Price: {price}€
-          </p>
+    <Link to={isbn}>
+      <article>
+        <div className='book-poster'>
+          <img src={cover} alt={title} />
+          <div className='book-info'>
+            <p>
+              Author(s): <span>{authorsWithSpace}</span>
+              <br />
+              Genre: <span>{genre}</span>
+              <br />
+              Publication date: <span>{publicationDate}</span>
+              <br />
+              Price: <span>{price}€</span>
+            </p>
+          </div>
         </div>
-      </div>
-      <div className='book-title'>
-        <h2>{title}</h2>
-      </div>
-    </article>
+        <div className='book-title'>
+          <h2>{title}</h2>
+        </div>
+      </article>
+    </Link>
   );
 };
 
